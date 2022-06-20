3bffff5a38c7813f12fde643f54b77ecda5d6332
Milena Guedes <milena@hibrido.com.br>
2022-06-20 14:39:30 -0300

    fixed routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,16 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import { MainPage } from "./components/MainPage";
 import { WelcomeSection } from "./components/WelcomeSection";
 import { MedalsSection } from "./components/MedalsSection";
 
 export const RoutesSection = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/idoso-seguro/" element={<WelcomeSection />} />
         <Route path="/idoso-seguro/jogar" element={<MainPage />} />
         <Route path="/idoso-seguro/medalhas" element={<MedalsSection />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
